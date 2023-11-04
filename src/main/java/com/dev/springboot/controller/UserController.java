98e4b197a2e9eb98eb7f38c6495ac0b483f9cce0
Bachir <bafosth@yahoo.fr>
2023-11-03 20:42:56 -0400

    created the request to get all the users, and update a user

--- src/main/java/com/dev/springboot/controller/UserController.java
+++ src/main/java/com/dev/springboot/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -23,9 +25,16 @@ public class UserController {
 
     // create get a user REST API
     @GetMapping("{id}")
-    public ResponseEntity<User> getUserById(Long userId) {
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId) {
         User getUser = userService.getUserById(userId);
         return new ResponseEntity<>(getUser, HttpStatus.OK);
+    }
+
+    // create get all the users REST API
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = userService.getAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/dev/springboot/service/UserService.java
+++ src/main/java/com/dev/springboot/service/UserService.java
@@ -2,8 +2,14 @@ package com.dev.springboot.service;
 
 import com.dev.springboot.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     User createUser(User user);
 
     User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
 }
